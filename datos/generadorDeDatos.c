5d60d22f9526daa9244b5b06b021997d9231c729
Juan Manuel Sardoy <jmsardoy@gmail.com>
2016-06-13 13:04:30 +0000

    fixed generacionDeDatos.c

--- datos/generadorDeDatos.c
+++ datos/generadorDeDatos.c
@@ -4,9 +4,15 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/file.h>
 #include <fcntl.h>
 #include <time.h>
 #define TAM 256
+
+char *DATA = "data.csv";
+char *DATAGEN = "dataGen.csv";
+char *LASTDATA = "lastData.csv";
+
 
 char* strTruncByChar(char *str, char c);
 char* strcut(char *str, int begin,int end);	
@@ -15,29 +21,49 @@ int main(int argc, char const *argv[])
 {
 	FILE* dataFile;
 	FILE* lastData;
-	FILE* dataGenFile;
+	FILE* dataGenFile = fopen(DATAGEN, "w");
+	fclose(dataGenFile);
 	char buffer[TAM];
-	
+	int fileHandler;
+
 	while(1){
 		
-		dataFile = fopen("data.csv", "r");
+		dataFile = fopen(DATA, "r");
 
 		//abre el archivo de datos dataGen en append
-		dataGenFile = fopen("dataGen.csv", "a");
-		//lee la primera linea y no la guarda porque ya esta guardaa
+		dataGenFile = fopen(DATAGEN, "a");
+		//lee la primera linea y no la guarda porque ya esta guardada
+		fileHandler = fileno(dataGenFile);
+		//obtiene exclusion mutua del archivo
+		flock(fileHandler, LOCK_EX);
 		fgets(buffer, TAM, dataFile);
+		//libera exclusion mutua del archivo
+		flock(fileHandler, LOCK_UN);
 		fclose(dataGenFile);
 
 		while(fgets(buffer, TAM, dataFile)){
 
 			strcpy(buffer,strTruncByChar(buffer,'\n'));
 			//abre el archivo de datos dataGen en append
-			dataGenFile = fopen("dataGen.csv", "a");
+			dataGenFile = fopen(DATAGEN, "a");
 			//abre el archivo lastData en modo write
-			lastData = fopen("lastData.csv", "w");	
+			lastData = fopen(LASTDATA, "w");	
 			printf("%s\n", buffer);
+
+			fileHandler = fileno(dataGenFile);
+			//obtiene exclusion mutua del archivo
+			flock(fileHandler,LOCK_EX);
 			fprintf(dataGenFile, "%s\n", buffer);
+			//libera exclusion mutua del archivo
+			flock(fileHandler,LOCK_UN);
+
+			fileHandler = fileno(lastData);
+			//obtiene exclusion mutua del archivo
+			flock(fileHandler,LOCK_EX);
 			fprintf(lastData, "%s\n", buffer);
+			//libera exclusion mutua del archivo
+			flock(fileHandler,LOCK_UN);
+
 			fclose(lastData);
 			fclose(dataGenFile);
 			sleep(1);
